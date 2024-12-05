4e3a4f6d64e1a83977008c885b7728fe1710c39f
KIRAN TALWAR <kirantalwar@KIRANs-MacBook-Air.local>
2024-12-05 08:37:13 -0500

    new change

--- src/main/java/Practice_Automation/Automation_Form12.java
+++ src/main/java/Practice_Automation/Automation_Form12.java
@@ -1,90 +1,62 @@
 package Practice_Automation;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Automation_Form12 {
-   
-        // Set up ChromeDriver path
-    	public static void main(String[] args) throws InterruptedException {
-            
-            WebDriverManager.chromedriver().setup();
-            
-            WebDriver driver = (WebDriver) new ChromeDriver();
+
+    public static void main(String[] args) throws InterruptedException {
+        // Set up WebDriver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
 
         
             driver.get("https://www.saucedemo.com/");
-            driver.manage().window().maximize();
 
-            // Fill the Name field
-            WebElement nameField = driver.findElement(By.xpath("//input[@id='user-name']"));
+            // Find and interact with the username field
+            WebElement nameField = driver.findElement(By.id("user-name"));
             nameField.sendKeys("standard_user");
-            Thread.sleep(3000);
-            
-            
 
-            // Fill the Password field
-            WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+            // Find and interact with the password field
+            WebElement passwordField = driver.findElement(By.id("password"));
             passwordField.sendKeys("secret_sauce");
-            Thread.sleep(3000);
-           
-            WebElement clickButton = driver.findElement(By.xpath("//input[@id='login-button']"));
-            
-            clickButton.click();
-          
-            Thread.sleep(3000);
-            
-            
-            WebElement menuItems = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
-            
-            menuItems.click();
-            
-            
-            
-           
 
-    
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-            WebElement sideBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//nav[@class='bm-item-list']//a")));
+            // Find and click the login button
+            WebElement loginButton = driver.findElement(By.id("login-button"));
+            loginButton.click();
+
+            // Simulate a wait to observe the action (replace with WebDriverWait if necessary)
+            Thread.sleep(2000); // Wait for 2 seconds
+            WebElement optionMenu = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
             
-            Thread.sleep(3000);
+             optionMenu.click();
             
+            Thread.sleep(2000); 
             
-            WebElement allAbout = driver.findElement(By.xpath("//a[@id='about_sidebar_link']"));
-            
-            allAbout.click();
-            
-           
-            driver.navigate().back();
-            
-            Thread.sleep(3000);
- WebElement menuItems1 = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
-            
-            menuItems1.click();
-            
-            
-            
+            WebElement aboutInfo = driver.findElement(By.xpath("//a[@id='about_sidebar_link']"));
+            aboutInfo.click();
             Thread.sleep(2000);
             
-            WebElement logout = driver.findElement(By.xpath("//a[@id='logout_sidebar_link']"));
+            driver.navigate().back();
             
-            logout.click();
+            WebElement optionMenu1 = driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']"));
             
+            optionMenu1.click();
             
+            Thread.sleep(2000);
+             WebElement logout = driver.findElement(By.xpath("//a[@id='logout_sidebar_link']"));
             
+              logout.click();
             
-        
-            
+         
 
-    	}
-}
+        
+            // Close the browser after execution
+            driver.quit();
+        }
+    }
 
